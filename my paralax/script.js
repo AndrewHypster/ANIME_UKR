fad8bf50feb194110bab174543afe85452e0c211
Андрій Гречух <119750751+AndrewHypster@users.noreply.github.com>
2023-07-12 00:55:20 +0300

    Update script.js

--- my paralax/script.js
+++ my paralax/script.js
@@ -1,1 +1,26 @@
-console.log('тупіца')
+const lists = document.querySelector('.lists')
+const content = document.querySelector('.paralax_content')
+const big_title = document.querySelector('.big_title')
+const gr_wrapper = document.querySelector('.gr_wrapper')
+
+content.style = `
+  width: ${lists.offsetWidht - (content.offsetHeight*4/100)*2}px;
+  transform: translate3d(0px, -${(content.offsetHeight/2)+(lists.offsetHeight/1.5)}px, 0px);
+  height: ${lists.offsetHeight}px;
+`
+
+gr_wrapper.style = `
+  margin-left: ${gr_wrapper.offsetWidth+gr_wrapper.offsetWidth+gr_wrapper.offsetWidth/5}px;
+  transform: translate3d(0px, ${-gr_wrapper.offsetHeight/3}px, 0px);
+`
+
+let index = -lists.offsetHeight
+const lists_paralax = () => {
+  if (index === lists.offsetHeight) {
+    index = -lists.offsetHeight
+  }
+  lists.style = `
+    transform: translate3d(0px, ${index++}px, 0px);
+  `
+}
+setInterval(lists_paralax, 8);
